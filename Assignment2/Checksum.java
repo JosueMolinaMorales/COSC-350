75982386c16aef42d4a55828c6f757946a8d4475
Josue Morales <molinajosue92@hotmail.com>
2022-03-31 15:26:25 -0400

    Updates to hw

--- Assignment2/Checksum.java
+++ Assignment2/Checksum.java
@@ -23,10 +23,9 @@ public class Checksum {
                 }
             }
             int sum = computeChecksum(hex);
-            // System.out.println("Sum is: " + sum + " In Binary: " + Integer.toBinaryString(sum));
-            // System.out.println(hex);
+            System.out.println("Sum is: " + sum + " In Binary: " + Integer.toBinaryString(sum));
+
             verifyChecksum(hex, sum); // This should be valid
-            //System.out.println("\n\n\n" + hex.replace('a', '0'));
             verifyChecksum(hex.replaceFirst("a","0"), sum); // This should be invalid
         } catch (Exception e) {
             e.printStackTrace();
@@ -45,12 +44,13 @@ public class Checksum {
             sum += Integer.parseInt(hex.substring(i, i+2), 16); 
             sum = (sum > MOD) ? (sum%MOD) + 1 : sum;
         }
-        System.out.println("Sum is: " + sum + " In Binary: " + Integer.toBinaryString(sum));
-        System.out.println("One's Complement is: " + (~sum) + " In Binary: " + Integer.toBinaryString(~sum));
-        System.out.println("SUM: " + Integer.toBinaryString(sum+ (~sum)));
         return sum;
     }
-
+    /**
+     * Verify the checksum
+     * @param hex The hex to compute the checksum
+     * @param checksum The sum calculated by the sender
+     */
     public static void verifyChecksum(String hex, int checksum) {
         System.out.println((~checksum+computeChecksum(hex) == -1) ? "Checksum is valid" : "Checksum is not valid");
     }
