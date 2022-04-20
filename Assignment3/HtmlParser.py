ef6b4cf6932f7947e626fbeb2321c2bd54e50d1b
Josue Morales <molinajosue92@hotmail.com>
2022-04-20 17:11:32 -0400

    Updates

--- Assignment3/HtmlParser.py
+++ Assignment3/HtmlParser.py
@@ -1,24 +1,28 @@
 from html.parser import HTMLParser
 
+count = 0
+outFile = open('Professors.txt', 'w')
 class HtmlParser(HTMLParser):
 
     def handle_starttag(self, tag: str, attrs) -> None:
-        pass
+        if tag == 'td': # This is a column within a row
+            pass
+        elif tag == 'table':
+            outFile.write('TABLE START')
 
     def handle_endtag(self, tag: str) -> None:
         #print("Encountered an end tag :", tag)
-        pass
+        if tag == 'tr':
+            outFile.write('\n\n')
+            print("END OF TR TAG\n\n")
+        elif tag == 'table':
+            outFile.write('TABLE END')
+            print("END OF TABLE\n\n")
 
     def handle_data(self, data: str) -> None:
         if not data.isspace():
-            if self.lasttag == 'p':
-                print("Encountered some data  :", data)
-            if self.lasttag == 'strong': # Name of the professor
-                print("Name of prof: " + data)
-            if self.lasttag == 'a':
-                print(f"{data=}")
-            if self.lasttag == 'td': # expertise
-                print(f"{data=}")
+            if self.lasttag in['p', 'strong', 'a', 'td', 'b']:
+                outFile.write(data)
 
     def feed(self, data: str) -> None:
         return super().feed(data)
--- Assignment3/webScraper.py
+++ Assignment3/webScraper.py
@@ -1,19 +1,47 @@
 import requests
 from HtmlParser import HtmlParser
 from bs4 import BeautifulSoup
+from html.parser import HTMLParser
 
-URL = "https://www.towson.edu/fcsm/departments/computerinfosci/facultystaff/"
-headers = {
-    'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36'
-}
-s = requests.get(URL,headers=headers)
-# print(s.text)
-# print(s.headers)
-# print(s.status_code)
-# print(s.cookies)
-# print(s.is_redirect)
-soup = BeautifulSoup(s.text,"html.parser")
-parser = HtmlParser()
+tableCount = 0
+outFile = open('Professors.txt', 'w')
+tableNames = ['Full Time Faculty', 'Full Time Staff', 'Faculty Emeriti']
+class HtmlParser(HTMLParser):
 
-for table in soup.findAll('tr'):
-    parser.feed(str(table))    
+    def handle_starttag(self, tag: str, attrs) -> None:
+        if tag == 'table':
+            outFile.write(tableNames[tableCount]+'\n')
+
+    def handle_endtag(self, tag: str) -> None:
+        global tableCount
+        if tag == 'tr':
+            outFile.write('\n\n')
+        elif tag == 'table':
+            outFile.write('END' + tableNames[tableCount] + '\n\n')
+            tableCount += 1
+
+    def handle_data(self, data: str) -> None:
+        if not data.isspace() and self.lasttag in ['p', 'strong', 'a', 'td', 'b', 'span']:
+                outFile.write(data + "\n")
+
+    def feed(self, data: str) -> None:
+        return super().feed(data)
+
+def main():
+    URL = "https://www.towson.edu/fcsm/departments/computerinfosci/facultystaff/"
+    headers = {
+        'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36'
+    }
+    s = requests.get(URL,headers=headers)
+
+    soup = BeautifulSoup(s.text,"html.parser")
+    parser = HtmlParser()
+
+    for tableRow in soup.findAll('table'):
+        parser.feed(str(tableRow))    
+
+    outFile.close()
+    print('./Professors.txt has been created successfully')
+
+if __name__ == '__main__':
+    main()
